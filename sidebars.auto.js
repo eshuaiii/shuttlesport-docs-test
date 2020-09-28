1e237a3a5d0d258b04e4915779c3bad0015358ce
Eric <63947753+eshuaiii@users.noreply.github.com>
2020-09-27 20:26:48 -0700

    Deploy website - based on

---
+++ sidebars.auto.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  docs: [
+    {
+        "type": "category",
+        "label": "Motor Fitness Training",
+        "items": [
+            "motor-fitness-training/badminton-movement-motor",
+            "motor-fitness-training/introduction",
+            "motor-fitness-training/philosophy",
+            "motor-fitness-training/conclusion"
+        ]
+    },
+    {
+        "type": "category",
+        "label": "Introduction",
+        "items": [
+            "Test/doc1",
+            "Test/doc2",
+            "Test/doc3",
+            "Test/mdx",
+            "Test/doc1"
+        ]
+    }
+]
+};
